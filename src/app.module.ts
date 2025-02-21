44acbd629f043e99a7859ed6337216a08bc77d4b
Algoruu <zlfkcmspdbdp@naver.com>
2025-02-21 12:17:36 +0900

    [Feat] Kopis 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { TwitterModule } from './modules/twitter/twitter.module';
 import { MapleModule } from './modules/maple/maple.module';
 import { ConfigModule } from '@nestjs/config';
+import { KopisModule } from './modules/kopis/kopis.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     TwitterModule,
     MapleModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    KopisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
