319aabd4056d0141abfc5552ba29f6d1a7296a5b
Algoruu <zlfkcmspdbdp@naver.com>
2025-02-25 16:48:25 +0900

    [Feat] LoginDto 추가

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,1 +1,19 @@
-export class LoginDto {}
+import { IsEmail, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginDto {
+  @ApiProperty({
+    example: 'ManSa1Tong@mail.com',
+    description: '사용자의 이메일 주소',
+  })
+  @IsEmail({}, { message: '올바른 이메일 형식이어야 합니다.' })
+  email: string;
+
+  @ApiProperty({
+    example: 'test123!',
+    description: '사용자의 비밀번호',
+  })
+  @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
+  @Length(8, 20, { message: '비밀번호는 8자 이상 20자 이하로 입력해주세요.' })
+  password: string;
+}
