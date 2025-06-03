597249f92e81847bf33d894d7e5cf51c75051bf8
Kim-Hyunhee <tta_bi@naver.com>
2025-06-03 13:20:05 +0900

    [Refactor] 필요 없는 폴더 삭제
    - maple, 영화 관련 모듈 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,17 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { TwitterModule } from './modules/twitter/twitter.module';
-import { MapleModule } from './modules/maple/maple.module';
 import { ConfigModule } from '@nestjs/config';
-import { MovieModule } from './modules/movie/movie.module';
 import { KopisModule } from './modules/kopis/kopis.module';
 import { BookModule } from './modules/book/book.module';
 import { SteamModule } from './modules/steam/steam.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
     TwitterModule,
-    MapleModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    MovieModule,
     KopisModule,
     BookModule,
     SteamModule,
